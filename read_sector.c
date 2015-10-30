4421214ab705d4f5ff6b3bbc99a2864ccdbb240c
coltonpierce <coltonpierce@gmail.com>
2015-10-30 12:40:48 -0500

    more stuff part3is

--- read_sector.c
+++ read_sector.c
@@ -1,3 +1,12 @@
 #include "main.h"
 #include "port.h"
 #include "read_sector.h"
+
+
+uint8_t Read_Sector(uint32_t sector_number, uint16_t sector_size, uint8_t *array_for_data)
+{
+	ncs=0;
+	Send_command(17, Sector_number);
+	Read_block()bytespersector, array_name);
+	ncs=1;
+}
