3ec7c34810ca7e5b5e8036a4cfa9796b2960de4b
coltonpierce <coltonpierce@gmail.com>
2015-11-02 13:15:22 -0600

    changed a ton of stuff
    we needed to use his directory functions not read sector to store all
    this.

--- Directory_Functions.h
+++ Directory_Functions.h
@@ -6,6 +6,7 @@
 //------- Public Constants  -----------------------------------------
 #define FAT32 (4)
 #define FAT16 (2)
+#define FAT12 (12)
 #define FAT32_shift (2)
 #define FAT16_shift (1)
 #define Disk_Error (0xF0)
@@ -15,22 +16,22 @@
 #define directory_bit  (0x10000000)  // lsb of first nibble (bit28)
 
 // ------ Public function prototypes -------------------------------
-//uint8 read8(uint16 offset, uint8 * array_name);
+uint8_t read8(uint16_t offset_address, uint8_t * array_name);
 
-//uint16 read16(uint16 offset, uint8 * array_name);
+uint16_t read16(uint16_t offset_address, uint8_t * array_name);
 
-//uint32 read32(uint16 offset, uint8 * array_name);
+uint32_t read32(uint16_t offset_address, uint8_t * array_name);
 
-//uint8 Mount_Drive(uint8 xdata * array_name);
+uint8_t mount_drive(void);
 
-//uint32 First_Sector (uint32 Cluster_num);
+uint32_t first_sector (uint32_t Cluster_num);
 
 uint16_t Print_Directory(uint32_t Sector_num, uint8_t xdata * array_in);
 
 uint32_t Read_Dir_Entry(uint32_t Sector_num, uint16_t Entry, uint8_t xdata * array_in);
 
-//uint32 Find_Next_Clus(uint32 Cluster_num, uint8 xdata * array_name);
+//uint32_t Find_Next_Clus(uint32_t Cluster_num, uint8_t xdata * array_name);
 
-//uint8 Open_File(uint32 Cluster, uint8 xdata * array_in);
+//uint8_t Open_File(uint32_t Cluster, uint8_t xdata * array_in);
 
 #endif
--- read_sector.c
+++ read_sector.c
@@ -6,19 +6,19 @@
 #include <stdio.h>
 #include "stdio.h"
 
-
-	uint16_t	BPB_BytesPerSec;
-	uint8_t   BPB_SecPerClus;
-	uint16_t	BPB_RsvdSecCnt;
-	uint8_t	  BPB_NumFATs;
-	uint16_t	BPB_RootEntCnt;
-	uint16_t	BPB_TotSec16;
-	uint16_t	BPB_FATSz16;
-	uint32_t	BPB_HiddenSec;
-	uint32_t	BPB_TotSec32;
-	uint32_t	BPB_FATSz32;
-	uint32_t	BPB_Root_Clus;
-
+	
+//	uint16_t	BPB_BytesPerSec;
+//	uint8_t   BPB_SecPerClus;
+//	uint16_t	BPB_RsvdSecCnt;
+//	uint8_t	  BPB_NumFATs;
+//	uint16_t	BPB_RootEntCnt;
+//	uint16_t	BPB_TotSec16;
+//	uint16_t	BPB_FATSz16;
+//	uint32_t	BPB_HiddenSec;
+//	uint32_t	BPB_TotSec32;
+//	uint32_t	BPB_FATSz32;
+//	uint32_t	BPB_Root_Clus;
+//	uint32_t	FirstFirstDataSec=()
 
 
 
@@ -32,157 +32,168 @@ uint8_t read_sector(uint32_t sector_number, uint16_t sector_size, uint8_t *array
 	return error_flag;	
 }
 
-uint32_t read_value_32(uint16_t offset_address, uint8_t *array_name)
-{
-	if (offset_address <512)
-	{
-		uint32_t return_value =0;
-		uint8_t temp, index;
-				for (index = 0; index < 4; index++)
-		{
-		temp =*(array_name + offset_address + ( 3 - index));
-			return_value= return_value << 8;
-			return_value |= temp;
-		}
-		printf("%lu",return_value);
-		return return_value;
-	}
-	else
-		return OFFSET_ERROR;
-}
+//uint32_t read32(uint16_t offset_address, uint8_t *array_name)
+//{
+//	if (offset_address <512)
+//	{
+//		uint32_t return_value =0;
+//		uint8_t temp, index;
+//				for (index = 0; index < 4; index++)
+//		{
+//		temp =*(array_name + offset_address + ( 3 - index));
+//			return_value= return_value << 8;
+//			return_value |= temp;
+//		}
+//		printf("%lu",return_value);
+//		return return_value;
+//	}
+//	else
+//		return OFFSET_ERROR;
+//}
 
-uint16_t read_value_16(uint16_t offset_address, uint8_t *array_name)
-{
-	uint16_t return_value =0;
-	uint8_t temp, index;
-	if(offset_address < 512)
-	{
-		for (index = 0; index < 2; index++)
-		{
-			temp =*(array_name + offset_address + ( 1 - index));
-			return_value= return_value << 8;
-			return_value |= temp;
-		}
-		printf("%u",return_value);
-		return return_value;
-	}
-	else
-	{
-		return OFFSET_ERROR;
-	}
-}
+//uint16_t read16(uint16_t offset_address, uint8_t *array_name)
+//{
+//	uint16_t return_value =0;
+//	uint8_t temp, index;
+//	if(offset_address < 512)
+//	{
+//		for (index = 0; index < 2; index++)
+//		{
+//			temp =*(array_name + offset_address + ( 1 - index));
+//			return_value= return_value << 8;
+//			return_value |= temp;
+//		}
+//		printf("%u",return_value);
+//		return return_value;
+//	}
+//	else
+//	{
+//		return OFFSET_ERROR;
+//	}
+//}
 
-uint8_t read_value_8(uint16_t offset_address, uint8_t *array_name)
-{
-	uint8_t return_value =0;
-	uint8_t temp;
-	if(offset_address < 512)
-	{
-		temp =*(array_name + offset_address);
-		return_value= return_value << 8;
-		return_value |= temp;
-		printf("%bu",return_value);
-		return return_value;
-	}
-	else
-	{
-		return OFFSET_ERROR;
-	}
-}
+//uint8_t read8(uint16_t offset_address, uint8_t *array_name)
+//{
+//	uint8_t return_value =0;
+//	uint8_t temp;
+//	if(offset_address < 512)
+//	{
+//		temp =*(array_name + offset_address);
+//		return_value= return_value << 8;
+//		return_value |= temp;
+//		printf("%bu",return_value);
+//		return return_value;
+//	}
+//	else
+//	{
+//		return OFFSET_ERROR;
+//	}
+//}
 
-uint8_t mount_drive(void)
-{
-	uint8_t sector[512];
-	uint8_t error_flag, rootDirSector;
-	uint16_t numofFATSectors;
-	uint32_t bpb_sector, FATSz, totSec, FATtype, countofClusters, dataSec;
-	
-	error_flag = NO_ERRORS;
-	
-	read_sector(0, 512, sector);
-	if(!(read8(0, sector) == 0xEB || read8(0, sector) == 0xE9))
-	{
-		bpb_sector = read32(0x01C6, sector);
-	}
-	else
-	{
-		error_flag = WRONG_RETURN;
-	}
-	if(error_flag == NO_ERRORS)
-	{
-		read_sector(bpb_sector, 512, sector);
-		
-		BPB_BytesPerSec = read16(0x000B, sector);
-		BPB_SecPerClus  =  read8(0x000D, sector);
-		BPB_RsvdSecCnt	= read16(0x000E, sector);
-		BPB_NumFATs			=  read8(0x0010, sector);
-		BPB_RootEntCnt	= read16(0x0011, sector);
-		BPB_TotSec16		= read16(0x0013, sector);
-		BPB_FATSz16			= read16(0x0016, sector);
-		BPB_HiddenSec		= read32(0x001C, sector);
-		BPB_TotSec32		= read32(0x0020, sector);
-		BPB_FATSz32			= read32(0x0024, sector);
-		BPB_Root_Clus		= read32(0x002C, sector);
-		
-		
-		rootDirSector = ((BPB_RootEntCnt * 32) + (BPB_BytesPerSec - 1)) / BPB_BytesPerSec;
-		
-		if(BPB_FATSz16 == 0)
-		{
-			FATSz = BPB_FATSz32;
-			printf("%-20s", "FAT size32 is ");
-			printf("%8.8bX", FATSz);
-		}
-		else
-		{
-			FATSz = BPB_FATSz16;
-			error_flag = WRONG_FAT_TYPE;
-		}
-			
-		if(BPB_TotSec16 == 0)
-		{
-			totSec = BPB_TotSec32;
-			printf("%-20s", "TotSec32 is ");
-			printf("%8.8bX", totSec);
-		}
-		else
-		{
-			totSec = BPB_TotSec16;
-			error_flag = WRONG_FAT_TYPE;
-		}
-		numofFATSectors = FATSz * BPB_NumFATs;  // Dont think this is used.
-		printf("%-20s", "numofFATSectors is ");
-		printf("%8.8bX", numofFATSectors);
-		dataSec = totSec - (BPB_RsvdSecCnt + (BPB_NumFATs * FATSz) + rootDirSector);
-		printf("%-20s", "dataSec is ");
-		printf("%8.8bX", dataSec);
-		countofClusters = dataSec / BPB_SecPerClus;
-		printf("%-20s", "countofClusters is ");
-		printf("%8.8bX", countofClusters);
-		if(countofClusters < 4085) 
-		{
-			FATtype = FAT12;
-			error_flag = WRONG_FAT_TYPE;
-		}
-		else if (countofClusters < 65525)
-		{	
-			FATtype = FAT16;
-			error_flag = WRONG_FAT_TYPE;
-		}
-		else 
-		{
-			FATtype = FAT32;
-		}
-		
-		printf("%-12s", "FAT size is ");
-		printf("%2d", FATtype);
-	}
-	
-	return error_flag;
-}
-	
+//uint8_t mount_drive(void)
+//{
+//	uint8_t sector[512];
+//	uint8_t error_flag, rootDirSector;
+//	uint16_t numofFATSectors;
+//	uint32_t bpb_sector, FATSz, totSec, FATtype, countofClusters, FirstDataSec;
+//	
+//	error_flag = NO_ERRORS;
+//	
+//	read_sector(0, 512, sector);
+//	if(!(read8(0, sector) == 0xEB || read8(0, sector) == 0xE9))
+//	{
+//		bpb_sector = read32(0x01C6, sector);
+//	}
+//	else
+//	{
+//		error_flag = WRONG_RETURN;
+//	}
+//	if(error_flag == NO_ERRORS)
+//	{
+//		read_sector(bpb_sector, 512, sector);
+//		
+//		BPB_BytesPerSec = read16(0x000B, sector);
+//		BPB_SecPerClus  =  read8(0x000D, sector);
+//		BPB_RsvdSecCnt	= read16(0x000E, sector);
+//		BPB_NumFATs			=  read8(0x0010, sector);
+//		BPB_RootEntCnt	= read16(0x0011, sector);
+//		BPB_TotSec16		= read16(0x0013, sector);
+//		BPB_FATSz16			= read16(0x0016, sector);
+//		BPB_HiddenSec		= read32(0x001C, sector);
+//		BPB_TotSec32		= read32(0x0020, sector);
+//		BPB_FATSz32			= read32(0x0024, sector);
+//		BPB_Root_Clus		= read32(0x002C, sector);
+//		
+//		
+//		rootDirSector = ((BPB_RootEntCnt * 32) + (BPB_BytesPerSec - 1)) / BPB_BytesPerSec;
+//		
+//		if(BPB_FATSz16 == 0)
+//		{
+//			FATSz = BPB_FATSz32;
+//			printf("%-20s", "FAT size32 is ");
+//			printf("%8.8bX", FATSz);
+//		}
+//		else
+//		{
+//			FATSz = BPB_FATSz16;
+//			error_flag = WRONG_FAT_TYPE;
+//		}
+//			
+//		if(BPB_TotSec16 == 0)
+//		{
+//			totSec = BPB_TotSec32;
+//			printf("%-20s", "TotSec32 is ");
+//			printf("%8.8bX", totSec);
+//		}
+//		else
+//		{
+//			totSec = BPB_TotSec16;
+//			error_flag = WRONG_FAT_TYPE;
+//		}
+//		numofFATSectors = FATSz * BPB_NumFATs;  // Dont think this is used.
+//		printf("%-20s", "numofFATSectors is ");
+//		printf("%8.8bX", numofFATSectors);
+//		FirstDataSec = totSec - (BPB_RsvdSecCnt + (BPB_NumFATs * FATSz) + rootDirSector);
+//		printf("%-20s", "FirstDataSec is ");
+//		printf("%8.8bX", FirstDataSec);
+//		countofClusters = FirstDataSec / BPB_SecPerClus;
+//		printf("%-20s", "countofClusters is ");
+//		printf("%8.8bX", countofClusters);
+//		if(countofClusters < 4085) 
+//		{
+//			FATtype = FAT12;
+//			error_flag = WRONG_FAT_TYPE;
+//		}
+//		else if (countofClusters < 65525)
+//		{	
+//			FATtype = FAT16;
+//			error_flag = WRONG_FAT_TYPE;
+//		}
+//		else 
+//		{
+//			FATtype = FAT32;
+//		}
+//		
+//		printf("%-12s", "FAT size is ");
+//		printf("%2d", FATtype);
+//	}
+//	
+//	return error_flag;
+//}
+//	
 
-
+//uint32_t first_sector(uint32_t cluster_num)
+//{
+//	uint32_t first_sector;
+//	uint32_t N = cluster_num;
+//	if (cluster_num ==0)
+//		{
+//		
+//		}
+//	else
+//		first_sector=((N-2)*BPB_SecPerClus)+ FirstFirstDataSec;
+//			
+//}
 
 
 
